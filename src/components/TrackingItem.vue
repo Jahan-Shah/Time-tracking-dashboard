c146b1dcc7c59fa3df16bfbfd073cc9ea976ac94
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-05-23 18:36:26 +0500

    added styling

--- src/components/TrackingItem.vue
+++ src/components/TrackingItem.vue
@@ -13,7 +13,7 @@ async function fetchData() {
 }
 onMounted(fetchData);
 
-const getNameForClass = (item) => {
+const getTitle = (item) => {
   const className =
     item.title.charAt(0).toLowerCase() + item.title.substring(1);
   return className.replace(/\s+/g, "");
@@ -22,13 +22,9 @@ const getNameForClass = (item) => {
 
 <template>
   <div class="item__container">
-    <div
-      class="item__bg"
-      :class="getNameForClass(item)"
-      v-for="item in jsonData"
-    >
-      <img :src="`/icon-${getNameForClass(item)}.svg`" alt="" />
-      <div class="item">
+    <div class="item" :class="getTitle(item)" v-for="item in jsonData">
+      <img class="item__svg" :src="`/icon-${getTitle(item)}.svg`" alt="" />
+      <div class="item__content">
         <div class="heading">
           <h3>{{ item.title }}</h3>
           <img src="/icon-ellipsis.svg" alt="ellipsis" />
@@ -42,19 +38,69 @@ const getNameForClass = (item) => {
 
 <style scoped>
 .item__container {
+  max-width: 830px;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  gap: 30px;
-}
-
-.item__bg {
-  width: 255px;
+  grid-template-columns: repeat(auto-fit, 255px);
+  grid-gap: 30px;
 }
 
 .item {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  position: relative;
+  padding-top: 43px;
+  overflow: hidden;
+  border-radius: 0.8rem;
+}
+
+.item__svg {
+  align-self: flex-end;
+  width: fit-content;
+  position: absolute;
+  top: -5px;
+  right: 16px;
+}
+
+.heading {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.item__content {
+  padding: 20px 30px;
   color: white;
   background-color: var(--neutral-200);
+  z-index: 1;
+
+  border-radius: 0.8rem 0.8rem 0 0;
+}
+
+h3 {
+  font-size: 1.125rem;
+  font-weight: var(--fw-med);
+}
+
+h2 {
+  font-size: 3.5rem;
+  font-weight: var(--fw-reg);
+}
+
+p {
+  font-weight: var(--fw-reg);
+  font-size: 0.85rem;
+  color: var(--neutral-400);
+}
+
+@media (max-width: 539px) {
+  .item__container {
+    padding: 1.5rem;
+  }
+  .item {
+    width: 330px;
+    grid-column: span 1;
+  }
 }
 
 .work {
